cb5506f0a35b5d94234037442c6f1c2736a67e29
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-09-18 10:04:21 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Katelyn Curtiss
+# Setember 18
+# IPO Intro
+
+num1 = input('4/n')
+num2 = input('16/n')
+
+total = num1 + num2 
+
+print(f'First number enetered was: {num1}')
+print(f'Second number enetered was: {num2}')
+print()
+print(f'The sum of {num1} and {num2} is: {total}')
